68d0e4d1cf0df57003b4d04740ee9edf6dcd216b
VyIq <105567237+VyIq@users.noreply.github.com>
2022-08-27 18:32:47 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,8 +62,8 @@ class EchoBot(KikClientCallback):
             try:
               self.client.send_chat_message(chat_message.group_jid, ".spin")
             except KeyboardInterrupt:
-            print("All done!")
-            time.sleep(301)
+              print("All done!")
+              time.sleep(301)
 
     def on_is_typing_event_received(self, response: chatting.IncomingIsTypingEvent):
         print("[+] {} is now {}typing.".format(response.from_jid, "not " if not response.is_typing else ""))
